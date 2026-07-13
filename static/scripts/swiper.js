0c2b83f32cfae52f1d51e869b5b177c32df70d05
G1owken <bisenislam007@gmail.com>
2026-07-13 18:06:58 +0500

    pc version

--- static/scripts/swiper.js
+++ static/scripts/swiper.js
@@ -1,15 +1,26 @@
+let widthBase = 1440;
+
+if (window.innerWidth <= 767) {
+    widthBase = 375;
+} else if (window.innerWidth <= 1024) {
+    widthBase = 980;
+}
+
+const widthMultiplier = window.innerWidth / widthBase;
+
 const swiper1 = new Swiper('.swiper-my-slider', {
     loop: false,
     speed: 800,
     allowTouchMove: false,
     direction: 'vertical',
+    spaceBetween: 20 * widthMultiplier,
 });
 
 function nextSlide() {
     setTimeout(() => {
 
         if (swiper1.isEnd) {
-            swiper1.slideTo(0, 1500);
+            swiper1.slideTo(0, 500);
         } else {
             swiper1.slideNext();
         }
